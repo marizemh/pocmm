0af465d309ad2676510e8d49255f60dff6ea411b
Marize Mijares Hernandez <mmijares@nisum.com>
2024-03-30 22:47:12 -0400

    personas

--- src/main/webapp/resources/src/personas/App.js
+++ src/main/webapp/resources/src/personas/App.js
@@ -1,8 +1,4 @@
-// app.js
-
-// Import our custom CSS
-import '../scss/styles.scss';
-import bootstrap from 'bootstrap';
+import $ from "jquery";
 import Api from "./Api";
 import Personas from "./Personas";
 import { showError } from "../commons/Utils";
@@ -14,14 +10,4 @@ const config = {
 const api = new Api(config);
 const personas = new Personas({ idTablaPersonas: 'personas', api: api, ...config });
 
-function renderPersonas() {
-    api.obtenerPersonas()
-        .then(personas => {
-            personas.render(personas);
-        })
-        .catch(error => {
-            showError(error);
-        });
-}
-
-renderPersonas();
+personas.obtenerYRenderizarPersonas();
